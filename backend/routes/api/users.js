cc26b981a7cd8fe117baa52acbd4863a546a31bb
Son Hoang <sonquhoang@gmail.com>
2023-07-07 09:21:38 -0700

    Corrected some bugs found when I added User input validation on user login backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,31 +10,4 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-
-router.post('/',
-    '',
-    validateSignup,
-    async (req, res) => {
-        const { email, password, username } = req.body;
-
-        const hashedPassword = bcrypt.hashSync(password);
-        const user = await User.create({
-            email,
-            username,
-            hashedPassword
-        });
-
-        const safeUser = {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-        };
-
-        await setTokenCookie(res, safeUser);
-
-        return res.json({
-            user: safeUser
-        })
-    })
-
 module.exports = router;
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -4,7 +4,7 @@ const { validationResult } = require('express-validator');
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
-  const validationErrors = validationResult(req);
+    const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) { 
     const errors = {};
@@ -21,6 +21,4 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
-module.exports = {
-  handleValidationErrors
-};
+module.exports = { handleValidationErrors }

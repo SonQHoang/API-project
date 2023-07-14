53b6c9167f4a332382c4a1417ab46677a19459bb
Son Hoang <sonquhoang@gmail.com>
2023-07-13 23:42:09 -0700

    Found the bug, debugging

---
+++ backend/db/seeders/20230714064114-seed-review-images.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    return queryInterface.bulkInsert(options,[
+      {
+        reviewId: 1,
+        url: "www.amazing.com",
+      },
+      {
+        reviewId: 2,
+        url: "www.wow.com",
+      },
+      {
+        reviewId: 3,
+        url: "www.crazy.com",
+      },
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      reviewId: { [Op.in]: [1,2,3] }
+    }, {});
+  }
+};

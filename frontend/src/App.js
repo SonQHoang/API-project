93b79dcbd08ed32d641ff5dccbee7457383c04ca
Son Hoang <sonquhoang@gmail.com>
2023-07-31 15:34:59 -0700

    Added delete functionality to spots

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Switch, Route } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
@@ -7,18 +7,15 @@ import CreateSpotForm from './components/Forms/CreateSpotForm'
 import ManageSpots from './components/SpotsCRUD/manageSpots'
 import GetAllSpots from './components/SpotsCRUD/GetAllSpots'
 
-import { getAllSpots } from '../src/store/spots'
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
 
-  const spotsData = useSelector(state => state.spots.allSpots)
+  // const spotsData = useSelector(state => state.spots.allSpots)
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-
-    dispatch(getAllSpots())
   }, [dispatch]);
 
   return (
@@ -32,7 +29,7 @@ function App() {
         <Route path="/spots/manage" component={ManageSpots}>
         </Route>
         <Route exact path="/spots/all">
-          <GetAllSpots spots={spotsData}/>
+          <GetAllSpots/>
         </Route>
       </Switch>
     </>
--- frontend/src/components/SpotsCRUD/GetAllSpots.js
+++ frontend/src/components/SpotsCRUD/GetAllSpots.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 // import { useHistory } from 'react-router-dom';
-import { getAllSpots } from '../../store/spots';
+import { getAllSpots, deleteSpot, updateSpot } from '../../store/spots';
 import { useEffect } from 'react';
 
 
@@ -16,20 +16,28 @@ function GetAllSpots() {
         return Object.values(state.spots.allSpots)
     });
 
+    const handleDelete = (spotId) => {
+        dispatch(deleteSpot(spotId))
+    }
+
+    const handleUpdate = (spotId) => {
+        dispatch(updateSpot(spotId))
+    }
+
     return (
         <>
-        <h1>Your Spots</h1>
+            <h1>Your Spots</h1>
 
             {spots.map(spot => {
                 return (
-                    <>
                     <div key={spot.id}>
                         <h2>{spot.name}</h2>
                         <p>{`City: ${spot.city}`}</p>
                         <p>{`State: ${spot.state}`}</p>
                         <p>{`Country: ${spot.country}`}</p>
+                        <button onClick={() => handleDelete(spot.id)}>Delete</button>
+                        <button onClick={() => handleUpdate(spot.id)}>Update</button>
                     </div>
-                    </>
                 )
             })}
         </>

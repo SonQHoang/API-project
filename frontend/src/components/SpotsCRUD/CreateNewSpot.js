e6edc849f1ee2d954da22ad6808087fe112ff5f4
Son Hoang <sonquhoang@gmail.com>
2023-07-30 20:24:57 -0700

    Adjusted form for spots

--- frontend/src/components/SpotsCRUD/CreateNewSpot.js
+++ frontend/src/components/SpotsCRUD/CreateNewSpot.js
@@ -1,68 +1,130 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector, useHistory } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 // import '.Spots.css';
 
 //! 1. Form for collecting User Data on Creating a Spot
 const CreateSpot = () => {
   const dispatch = useDispatch();
-  const newSpotList = useSelector((state) => Object.values(state.newSpot));
-
-  const [spot, setSpot] = useState({
-    address: "",
-    city: "",
-    state: "",
-    country: "",
-    name: "",
-    description: "",
-    price: "",
-  });
+  const history = useHistory();
 
   //!9. Using useSelector for our component to listen to our changes in state
-  const newSpot = useSelector((state) => Object.values(state.newSpot))
+  const newSpotList = useSelector((state) => Object.values(state.newSpot));
+
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [country, setCountry] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [validationObject, setValidationObject] = useState({});
+
+  useEffect(() => {}, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Validate the fields of the spot object
-    if (
-      !spot.address ||
-      !spot.city ||
-      !spot.state ||
-      !spot.country ||
-      !spot.name ||
-      !spot.description ||
-      !spot.price
-    ) {
-      alert("All fields must be filled out.");
+    const errorsObject = {};
+    if (address === "") {
+      errorsObject.address = "You must submit an address";
+    }
+    if (city === "") {
+      errorsObject.city = "You must submit a city";
+    }
+    if (state === "") {
+      errorsObject.state = "You must submit a state";
+    }
+    if (country === "") {
+      errorsObject.country = "You must submit a country";
+    }
+    if (description === "") {
+      errorsObject.description = "You must submit a description";
+    }
+    if (price === "") {
+      errorsObject.price = "You must submit a price";
+    }
+    setValidationObject(errorsObject);
+
+    if (Object.keys(errorsObject).length > 0) {
       return;
     }
 
-    dispatch(createSpot(spot));
+    const spot = {
+      address,
+      city,
+      state,
+      country,
+      name,
+      description,
+      price,
+    };
+    dispatch(CreateSpot(spot));
+
+    setAddress("");
+    setCity("");
+    setState("");
+    setCountry("");
+    setName("");
+    setDescription("");
+    setPrice("");
+
+    history.push("/")
+  };
 
   return (
     <>
-    <h1>User's Spots</h1>
-    {newSpotList.map((spot) => (
-      <p key={spot.id}>{spot.name}</p>
-    ))}
-    <form onSubmit={handleSubmit}>
-      <input
-      type="text"
-      value={spot.address}
-      onChange={(e) => setSpot({...spot, address: e.target.value})}
-      placeholder="Address"
-      />
-      {/* for city, state, country, name, description, price */}
-      <input/> 
-      <input/>
-      <input/>
-      <input/>
-      <input/>
-      <input/>
-      <button type="submit">Create a New Spot</button>
-    </form>
-      </>
-  )
+      <h1>User's Spots</h1>
+      {newSpotList.map((spot) => (
+        <p key={spot.id}>{spot.name}</p>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          placeholder="Address"
+        />
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value )}
+          placeholder="City"
+        />
+        <input
+          type="text"
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+          placeholder="State"
+        />
+        <input
+          type="text"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+          placeholder="Country"
+        />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+        />
+        <input
+          type="text"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="Price"
+        />
+        <button type="submit">Create a New Spot</button>
+      </form>
+    </>
+  );
 };
-}
 
-export default CreateSpot
+export default CreateSpot;
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -2,24 +2,21 @@ const CREATE_NEW_SPOT = "spots/newSpot";
 
 //!6 Action Creator; Taking in info from Thunk Action Creator, sending to reducer
 const newSpot = (spot) => {
+  console.log('this is our spot data', spot)
   return {
     type: CREATE_NEW_SPOT,
-    spot,
     // payload here
+    payload: spot,
   };
 };
 //! 2 Thunk Action Creator for New Spots
 //! Going to make the fetch request to the backend to gather our data
 
-export const createSpot = (spotData) => async (dispatch) => {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
+export const createSpot = (payload) => async (dispatch) => {
   const options = {
     method: "POST",
-    headers,
-    body: JSON.stringify(spotData),
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
   };
   //! 5 Getting the data back, sending it up to normal action creator
   const response = await fetch("api/spots/newSpot", options);
@@ -33,24 +30,30 @@ export const createSpot = (spotData) => async (dispatch) => {
   }
 };
 
-//! 7 Taking in data from normal action creator
+//! 7 Moving to Reducer FROM normal action creator
 
-const initialState = {};
+const initialState = { spots: {}, singleSpot: {} };
 
 const newSpotReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case CREATE_NEW_SPOT: {
-        const newSpot = action.spot;
-        // If errors, check state with console logo HERE
-        if (!newSpot) return state;
-        return {
-          ...state,
+  switch (action.type) {
+    case CREATE_NEW_SPOT: {
+      //! Coming from our Redux Store Shape
+      const newSpot = action.payload;
+      return {
+        //? We create a shallow copy to make sure we don't mutate our original state
+        ...state,
+        
+        spots: {
+          //? We're creating a shallow copy of our original (current) state.spots, which is a new ref in memory
+          ...state.spots,
           [newSpot.id]: newSpot,
-        };
-      }
-      default: 
-      return state;
+        },
+        singleSpot: newSpot, 
+      };
     }
-}
+    default:
+      return state;
+  }
+};
 
 export default newSpotReducer;

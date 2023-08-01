bb4e7e3c7e12de5b2afe2d73758805d07aca30fd
Son Hoang <sonquhoang@gmail.com>
2023-08-01 09:20:36 -0700

    Got the form workingn again

--- frontend/src/components/Forms/CreateSpotForm.js
+++ frontend/src/components/Forms/CreateSpotForm.js
@@ -147,37 +147,42 @@ const CreateSpotsForm = ({spot, buttonText}) => {
         {validationObject.country && (
           <p className="errors">{validationObject.country}</p>
         )}
+        <h2>Describe your place to guests</h2>
+        <p>Mention the best features of your space, any special amenities like fast wifi or parking,and what you love about the  neighborhood</p>
         <label>
-          Name
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Name"
-          />
-        </label>
-        {validationObject.name && (
-          <p className="errors">{validationObject.name}</p>
-        )}
-        <label>
-          Description
+          Please write at least 30 characters
           <textarea
-            type="text"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            placeholder="Description"
+            placeholder="Please write at least 30 characters"
           />
         </label>
         {validationObject.description && (
           <p className="errors">{validationObject.description}</p>
-        )}
+          )}
+          <h2>Create a title for your spot</h2>
+          <p>Catch guests' attention with a post title that highlights what makes your place special.</p>
+          <label>
+            Name of your spot
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name of your spot"
+            />
+          </label>
+          {validationObject.name && (
+            <p className="errors">{validationObject.name}</p>
+          )}
+        <h2>Set a base price for your spot</h2>
+        <p>Competitive pricing can help your listing stand out and rank higher in search results.</p>
         <label>
-          Price
+          Price per night (USD)
           <input
-            type="text"
+            type="number"
             value={price}
             onChange={(e) => setPrice(e.target.value)}
-            placeholder="Price"
+            placeholder="Price per night (USD)"
           />
         </label>
         {validationObject.price && (

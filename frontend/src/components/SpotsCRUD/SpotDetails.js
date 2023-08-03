450c422677e58a93a7a72195183679c2fbb75b90
Son Hoang <sonquhoang@gmail.com>
2023-08-03 12:57:41 -0700

    Got the spot details styled a bit

--- frontend/src/components/SpotsCRUD/SpotDetails.js
+++ frontend/src/components/SpotsCRUD/SpotDetails.js
@@ -10,6 +10,13 @@ function SpotDetailsPage() {
     const spot = useSelector((state) => state.spots.singleSpot)
     // console.log('What does this spot look like?=====>', spot)
 
+    const spotPrice = spot?.price || 1;
+
+    const handleReserveClick = () => {
+        alert('Feature coming soon');
+    }
+
+
     useEffect(() => {
         dispatch(getSpotById(spotId))
     }, [dispatch, spotId])
@@ -19,18 +26,28 @@ function SpotDetailsPage() {
         <div>
             <h2>{`${spot.name}`}</h2>
             <div className="details-container">
-            <p>{`${spot.city}`}</p>
-            <p>{`${spot.state}`}</p>
-            <p>{`${spot.country}`}</p>
+                <p>{`${spot.city}`}</p>
+                <p>{`${spot.state}`}</p>
+                <p>{`${spot.country}`}</p>
             </div>
+
+
             <div>
-            {spot.largeImageUrl && <img className="large-image" src={spot.largeImageUrl} alt="Guitar" />}
-                        {spot.SpotImages && spot.SpotImages.map((image, index) => (
-                            <img key={index} className="small-image" src={image.url} alt={`Guitar ${index + 1}`} />
-                        ))}
+                {spot.largeImageUrl && <img className="large-image" src={spot.largeImageUrl} alt="Guitar" />}
+                {spot.SpotImages && spot.SpotImages.map((image, index) => (
+                    <img key={index} className="small-image" src={image.url} alt={`Guitar ${index + 1}`} />
+                ))}
             </div>
-            <p>{`Hosted by Son Hoang`}</p>
-            <p>{`Paragraph: ${spot.description}`}</p>
+            <div className="host-details-reserve-container">
+                <div>
+                    <p>{`Hosted by Son Hoang`}</p>
+                    <p>{`Paragraph: ${spot.description}`}</p>
+                </div>
+                <div className="callout-container">
+                    <p className="price">{`$${spotPrice} per night`}</p>
+                    <button onClick={handleReserveClick}>Reserve</button>
+                </div>
+            </div>
         </div>
     )
 }
